ee54d676c9a754de34a2b97432c939b70e481b34
Lacty <akira206@gmail.com>
2017-01-08 20:35:02 +0900

    update yml

--- XcodeCITest/main.cpp
+++ XcodeCITest/main.cpp
@@ -1,14 +1,6 @@
 
 #include <iostream>
-
-
-void fizzBuzz(int i, int end) {
-  if      (!(i % 15)) { std::cout << "FizzBuzz" << std::endl; }
-  else if (!(i % 3))  { std::cout << "Fizz"     << std::endl; }
-  else if (!(i % 5))  { std::cout << "Buzz"     << std::endl; }
-  else                { std::cout << i          << std::endl; }
-  if      (++i < end) { fizzBuzz(i, end); }
-}
+#include "fizzBuzz.h"
 
 int main() {
   fizzBuzz(1, 50);
