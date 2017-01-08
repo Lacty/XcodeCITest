4c6875ebd163c19ad0fd06036f0c127d627e0040
Lacty <akira206@gmail.com>
2017-01-08 17:47:40 +0900

    add project

---
+++ XcodeCITest/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+
+void fizzBuzz(int i, int end) {
+  if      (!(i % 15)) { std::cout << "FizzBuzz" << std::endl; }
+  else if (!(i % 3))  { std::cout << "Fizz"     << std::endl; }
+  else if (!(i % 5))  { std::cout << "Buzz"     << std::endl; }
+  else                { std::cout << i          << std::endl; }
+  if      (++i < end) { fizzBuzz(i, end); }
+}
+
+int main() {
+  fizzBuzz(1, 50);
+}
